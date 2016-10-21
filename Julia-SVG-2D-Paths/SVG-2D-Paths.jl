4ad12b50263cf116444754c1332b2937300b15a4
Jarvist Moore Frost <jarvist@gmail.com>
2016-10-21 14:50:49 +0100

    Initial commit for codes to draw 2D SVG repr. of path ints.

--- Julia-SVG-2D-Paths/SVG-2D-Paths.jl
+++ Julia-SVG-2D-Paths/SVG-2D-Paths.jl
@@ -1,4 +1,6 @@
 # Feynman 1D path-integral Paths drawn with SVG Paths!
+
+# Paths in Paths!
 # See here for spec / tutorial: https://developer.mozilla.org/en/docs/Web/SVG/Tutorial/Paths
 
 svgf=open("path.svg","w")
@@ -71,4 +73,18 @@ for i in 1:Paths
 end
 
 @printf(svgf,"\n</svg>\n")
+@printf(svgf,"<svg width=\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">\n")
+
+#//  <path d="M10 10"/>
+
+function printsegment(dx,dy)
+    @printf(svgf,"<path d=\"M l %f %f",dx,dy)
+end
+
+printsegment(10,10)
+
+function drawpath()
+    printsegment(10,10)
+end
+
 close(svgf)
