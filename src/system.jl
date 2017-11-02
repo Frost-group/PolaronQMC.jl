5360f07211fa9dfc99c6d0dbb6f347fcdb05a80e
Jarvist Moore Frost <jarvist@gmail.com>
2017-11-02 16:26:17 +0000

    Correct size for bounds; returns even fn.

--- src/system.jl
+++ src/system.jl
@@ -1,7 +1,7 @@
 # Initially targetting 1D system; potential + etc. on [0,1]
 
 function sampledV(V, N=50)
-    discreteV=[ V(i/N - 1/2) for i=0:N ]
+    discreteV=[ V(i/(N-1) - 1/2) for i=0:(N-1) ]
     return discreteV
 end
 
