1a3e8123edf7733afbbb82543328db8639a73e59
Jarvist Moore Frost <jarvist@gmail.com>
2021-03-31 13:31:20 +0100

    Update for Julia 1.0; testing, Project.toml

--- src/Halcyon.jl
+++ src/Halcyon.jl
@@ -1,8 +1,8 @@
-# HALCYON.jl
+# Halcyon.jl
 # Pigs might just fly.
 # Path Integral Density Inference Theory
 
-module HALCYON
+module Halcyon
 
 # Namespace is currently very dirty and everything will need to get (re)sorted
 # as it becomes clear where things should lie.
@@ -12,3 +12,4 @@ include("Schrodinger.jl") # discretised TISE solver
 include("PIMC.jl") # Path integrals for the win
 
 end # module
+
