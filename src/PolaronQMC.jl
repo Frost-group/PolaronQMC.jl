14a82e2da6715c0dff45d49d656784e7fdd193a2
Bradley Martin <bam116@ic.ac.uk>
2022-05-18 17:26:27 +0100

    Update PIMC to accept observables as input. Added draw_3d_path() to draw paths. Added animate_PIMC() to animate MC simulation.

--- src/PolaronQMC.jl
+++ src/PolaronQMC.jl
@@ -13,7 +13,7 @@ using Plots
 export Path, Potential, ConstantPotential, OneBodyPotential, TwoBodyPotential, HarmonicPotential, MexicanHatPotential, CoulombPotential, FrohlichPotential, Regime, Simple_Regime, Primitive_Regime
 
 # Export types
-export Path, Potential, ZeroPotential, OneBodyPotential, TwoBodyPotential, HarmonicPotential, CoulombPotential
+export Path, Potential, ConstantPotential, OneBodyPotential, TwoBodyPotential, HarmonicPotential, CoulombPotential
 
 # Export potentials
 export one_body_potential, two_body_potential
