d5e383a19f79eb0f36452f2e1ea02e7872dfa045
Bradley Martin <bam116@ic.ac.uk>
2022-05-18 17:26:27 +0100

    Update PIMC to accept observables as input. Added draw_3d_path() to draw paths. Added animate_PIMC() to animate MC simulation.

--- src/PIMC.jl
+++ src/PIMC.jl
@@ -53,11 +53,15 @@ function primitive_action(path::Path, bead::Int, particle::Int, potentials::Arra
     return primitive_action
 end
 
-function PIMC(n_steps::Int, movers, path::Path, potentials::Union{Potential, Array{Potential}})
+function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{Potential, Array{Potential}})
+	
 	observable_skip = 0.001 * n_steps
 	equilibrium_skip = 0.2 * n_steps
+	# equilibrium_skip = 0
+	
 	n_accepted = Dict(string(Symbol(mover)) => 0 for mover in movers)
-	energy_trace = []
+	observable_traces = Dict(string(Symbol(observable)) => [] for observable in observables)
+	
 	path_trace = []
 	for step in 1:n_steps
 
@@ -68,12 +72,50 @@ function PIMC(n_steps::Int, movers, path::Path, potentials::Union{Potential, Arr
 		end
 
 		if mod(step, observable_skip) == 0 && step > equilibrium_skip
-			append!(energy_trace, total_energy(path, potentials))
+			for observable in observables
+				append!(observable_traces[string(Symbol(observable))], [observable(path, potentials)])
+			end
 			append!(path_trace, [path.beads])
 		end
+		
 	end
 
 	acceptance_ratio = Dict(string(Symbol(mover)) => 1.0 * n_accepted[string(Symbol(mover))] / (n_steps * path.n_particles) for mover in movers)
 
-	return acceptance_ratio, energy_trace, path_trace
+	return acceptance_ratio, path_trace, observable_traces
+end
+
+function draw_beads_3d(path, xlims, ylims, zlims)
+
+	p = plot()
+	
+	for particle in 1:size(path)[2]
+		x = path[:, particle, 1]
+		y = path[:, particle, 2]
+		z = path[:, particle, 3]
+		
+		x = reshape(x, length(x))
+		y = reshape(y, length(y))
+		z = reshape(z, length(z))
+
+		push!(x, x[1])
+		push!(y, y[1])
+		push!(z, z[1])
+
+		plot!(p, x, y, z, marker = :circle, label = "P $particle", legend = false, xlims = xlims, ylims = ylims, zlims = zlims)
+	end
+	return p
+end
+
+function animate_PIMC(pimc)
+
+	xlims = [minimum([minimum(x[:, :, 1]) for x in pimc[2]]), maximum([maximum(x[:, :, 1]) for x in pimc[2]])]
+	ylims = [minimum([minimum(x[:, :, 2]) for x in pimc[2]]), maximum([maximum(x[:, :, 2]) for x in pimc[2]])]
+	zlims = [minimum([minimum(x[:, :, 3]) for x in pimc[2]]), maximum([maximum(x[:, :, 3]) for x in pimc[2]])]
+
+	animation = @animate for p in pimc[2]
+		draw_beads_3d(p, xlims, ylims, zlims)
+	end
+
+	return animation
 end
