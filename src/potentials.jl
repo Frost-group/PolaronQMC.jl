657dc7202daa3a695c3abcc7bd62cd4f4ca92a81
Bradley Martin <bam116@ic.ac.uk>
2022-05-18 16:32:31 +0100

    Replaced ZeroPotential with more general ConstantPotential
    Trying to solve UndefVarError between PIMC.jl and potential.jl files. Still not solved!

--- src/potentials.jl
+++ src/potentials.jl
@@ -48,15 +48,18 @@ function two_body_potential(potential::CoulombPotential, path::Path, bead::Int, 
     return -potential.κ / norm(path.beads[bead, particle_one, :] .- path.beads[bead, particle_two, :])
 end
 
+# For potentials that depend on two bodies.
 abstract type TwoBodyPotential <: Potential end
 
-struct ZeroPotential <: Potential 
-    n :: Float64
-    function ZeroPotential()
-        new(0.0)
+# A constant potential.
+struct ConstantPotential <: NoBodyPotential 
+    V :: Float64
+    function ConstantPotential(V::Float64)
+        new(V)
     end
 end
 
+# A Harmonic potential for a single body.
 struct HarmonicPotential <: OneBodyPotential
     ω :: Float64
     function HarmonicPotential(ω::Float64)
@@ -64,6 +67,7 @@ struct HarmonicPotential <: OneBodyPotential
     end
 end
 
+# Coulomb interaction between two bodies.
 struct CoulombPotential <: TwoBodyPotential
     κ :: Float64
     function CoulombPotential(κ::Float64)
@@ -71,18 +75,28 @@ struct CoulombPotential <: TwoBodyPotential
     end
 end
 
-function one_body_potential(potential::ZeroPotential, path::Path, bead::Int, particle::Int)
-    return 0.0
-end
+"""
+Outer constructors for different potential types.
+"""
 
+
+
+# Return the harmonic potential for a single particle.
 function one_body_potential(potential::HarmonicPotential, path::Path, bead::Int, particle::Int)
     return 0.5 * potential.ω^2 * norm(path.beads[bead, particle, :])^2
 end
 
-function two_body_potential(potential::ZeroPotential, path::Path, bead::Int, particle_one::Int, particle_two::Int)
-    return 0.0
+# Just return value of potential for a constant potential independent of single particle.
+function one_body_potential(potential::ConstantPotential, path::Path, bead::Int, particle::Int)
+    return potential.V
 end
 
+# Just return value of potential for a constant potential independent of two particles.
+function two_body_potential(potential::ConstantPotential, path::Path, bead::Int, particle_one::Int, particle_two::Int)
+    return potential.V
+end
+
+# Return the Coulomb potential between two particles.
 function two_body_potential(potential::CoulombPotential, path::Path, bead::Int, particle_one::Int, particle_two::Int)
     return -potential.κ / norm(path.beads[bead, particle_one, :] .- path.beads[bead, particle_two, :])
 end
