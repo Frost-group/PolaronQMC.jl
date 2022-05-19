8ea267ced4dbb506ade659a3187e44b864ce4ce4
Jarvist Moore Frost <jarvist@gmail.com>
2022-05-19 21:16:02 +0100

    Basic integration tests (no actual tests).

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,17 @@
-using PolaronQMC
-
 using Test
+
+using PolaronQMC
 
 @assert 1==1
 
-const T=300
-const tau=0.5
-nbeads=round(Int,1/(tau*T)) # ??? 
+#const T=300
+#const tau=0.5
+#nbeads=round(Int,1/(tau*T)) # ??? 
 
-nbeads=4
-nparticles=2
-spatialdimensions=1
+#nbeads=4
+#nparticles=2
+#spatialdimensions=1
 
-PolaronQMC.Path(nbeads, nparticles,  spatialdimensions=spatialdimensions)
+#PolaronQMC.Path(nbeads, nparticles,  spatialdimensions=spatialdimensions)
+@testset "SimpleIntegrationTest.jl" begin include("SimpleIntegrationTest.jl") end
 
