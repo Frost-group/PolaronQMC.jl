a4de9ddffdb46fc2d5649a06fe406edb1314c3af
Bradley Martin <bam116@ic.ac.uk>
2022-05-24 17:41:26 +0100

    Harmonic oscillator energy about right!
    - Need / n_beads in kinetic energy to get the correct equipartition result?
    - Played around with step size in moves!
    - Bit of meta-programming in PIMC(). PIMC() still ugly overall though.

--- src/PIMC.jl
+++ src/PIMC.jl
@@ -93,7 +93,7 @@ function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{P
 	equilibrium_skip = 0.1 * n_steps
 	
 	n_accepted = Dict(string(Symbol(mover!)) => 0 for mover! in movers)
-	observable_traces = Dict(string(Symbol(observable)) => [] for observable in observables)
+	# observable_traces = Dict(string(Symbol(observable)) => [] for observable in observables)
 	
 	for observable in observables
 		@eval $(Symbol(observable, "_statistics")) = Series(Trace(Mean()), Trace(AutoCov(0)))
@@ -116,23 +116,28 @@ function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{P
 			for observable in observables
 				@eval trace_symbol = $(Symbol(observable, "_statistics"))
 				trace_value = observable(path, potentials)
-				append!(observable_traces[string(Symbol(observable))], [trace_value])
+				# append!(observable_traces[string(Symbol(observable))], [trace_value])
 				fit!(trace_symbol, trace_value)
-				append!(observable_plots, [plot(trace_symbol, linestyle = :solid, title = "$observable")])
+				append!(observable_plots, [plot(trace_symbol, minorgrid = true, linestyle = :solid, title = "$observable", legendfontsize=5, markersize = 2.5)])
 			end
 			append!(path_trace, [path.beads])
 			ymax = ymax < maximum(path.beads[:, :, 1]) ? maximum(path.beads[:, :, 1]) : ymax
 			ymin = ymin > minimum(path.beads[:, :, 1]) ? minimum(path.beads[:, :, 1]) : ymin
-			path_plot = plot(1:path.n_beads, path.beads[:, :, 1], ylabel = "x position", xlabel = "imaginary time", ylims = [ymin, ymax], title = "Path", marker = :circle, legend = false)
+			path_plot = plot(1:path.n_beads, path.beads[:, :, 1], grid = false, ylabel = "x position", xlabel = "imaginary time", ylims = [ymin, ymax], title = "Path", marker = :circle, legend = false)
 			plot(observable_plots..., path_plot, size = (500, 500),  layout = @layout [a;b;c])
 			gui()
-			
 		end
-		
 	end
 
 	acceptance_ratio = Dict(string(Symbol(mover!)) => 1.0 * n_accepted[string(Symbol(mover!))] / (n_steps * path.n_particles) for mover! in movers)
+	
+	returns = [acceptance_ratio, path_trace]
 
-	return acceptance_ratio, path_trace, observable_traces
+	for observable in observables
+		@eval trace_symbol = $(Symbol(observable, "_statistics")) 
+		append!(returns, [trace_symbol])
+	end
+
+	return tuple(returns...)
 end
 
