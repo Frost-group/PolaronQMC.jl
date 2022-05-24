8bc35503293363d24c019503638a3b52af2c9980
Bradley Martin <bam116@ic.ac.uk>
2022-05-24 17:48:46 +0100

    Quick way to turn of visuals for now

--- src/PIMC.jl
+++ src/PIMC.jl
@@ -6,20 +6,21 @@ function relabel_beads!(path::Path)
 	path.beads = path.beads[slices, :, :]
 end
 
-function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{Potential, Array{Potential}})
+function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{Potential, Array{Potential}}; visual = true)
 	
 	observable_skip = 0.01 * n_steps
 	equilibrium_skip = 0.1 * n_steps
 	
 	n_accepted = Dict(string(Symbol(mover!)) => 0 for mover! in movers)
-	# observable_traces = Dict(string(Symbol(observable)) => [] for observable in observables)
 	
 	for observable in observables
 		@eval $(Symbol(observable, "_statistics")) = Series(Trace(Mean()), Trace(AutoCov(0)))
 	end
 
-	ymax = maximum(path.beads[:, :, 1])
-	ymin = minimum(path.beads[:, :, 1])
+	if visual
+		ymax = maximum(path.beads[:, :, 1])
+		ymin = minimum(path.beads[:, :, 1])
+	end
 
 	path_trace = []
 	for step in 1:n_steps
@@ -32,19 +33,25 @@ function PIMC(n_steps::Int, path::Path, movers, observables, potentials::Union{P
 
 		if mod(step, observable_skip) == 0 && step > equilibrium_skip
 			observable_plots = []
+
 			for observable in observables
 				@eval trace_symbol = $(Symbol(observable, "_statistics"))
 				trace_value = observable(path, potentials)
-				# append!(observable_traces[string(Symbol(observable))], [trace_value])
 				fit!(trace_symbol, trace_value)
-				append!(observable_plots, [plot(trace_symbol, minorgrid = true, linestyle = :solid, title = "$observable", legendfontsize=5, markersize = 2.5)])
+
+				if visual
+					append!(observable_plots, [plot(trace_symbol, minorgrid = true, linestyle = :solid, title = "$observable", legendfontsize=5, markersize = 2.5)])
+				end
 			end
+
 			append!(path_trace, [path.beads])
-			ymax = ymax < maximum(path.beads[:, :, 1]) ? maximum(path.beads[:, :, 1]) : ymax
-			ymin = ymin > minimum(path.beads[:, :, 1]) ? minimum(path.beads[:, :, 1]) : ymin
-			path_plot = plot(1:path.n_beads, path.beads[:, :, 1], grid = false, ylabel = "x position", xlabel = "imaginary time", ylims = [ymin, ymax], title = "Path", marker = :circle, legend = false)
-			plot(observable_plots..., path_plot, size = (500, 500),  layout = @layout [a;b;c])
-			gui()
+			if visual
+				ymax = ymax < maximum(path.beads[:, :, 1]) ? maximum(path.beads[:, :, 1]) : ymax
+				ymin = ymin > minimum(path.beads[:, :, 1]) ? minimum(path.beads[:, :, 1]) : ymin
+				path_plot = plot(1:path.n_beads, path.beads[:, :, 1], grid = false, ylabel = "x position", xlabel = "imaginary time", ylims = [ymin, ymax], title = "Path", marker = :circle, legend = false)
+				plot(observable_plots..., path_plot, size = (500, 500),  layout = @layout [a;b;c])
+				gui()
+			end
 		end
 	end
 
