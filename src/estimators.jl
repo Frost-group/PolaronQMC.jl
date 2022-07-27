e587802eb2a3d4c8f6a248dd498da6d745b2dbaa
Bradley Martin <bam116@ic.ac.uk>
2022-07-27 17:25:07 +0100

    PIMC and estimator fixes
    Energy sampling just at one link, not all links!

--- src/estimators.jl
+++ src/estimators.jl
@@ -2,11 +2,12 @@
 
 function kinetic_energy(path::Path, potentials)
 	kinetic_energy = 0.0
-	prefactor = 1.0 / (4.0 * path.λ * path.τ)
-	for bead in 1:path.n_beads, particle in 1:path.n_particles
+    bead = rand(1:path.n_beads)
+	prefactor = -1.0 / (4.0 * path.λ * path.τ^2)
+	for particle in 1:path.n_particles
 		kinetic_energy += norm(path.beads[bead, particle, :] - path.beads[bead-1, particle, :])^2
 	end
-	return prefactor * kinetic_energy / path.n_beads + path.n_dimensions * path.n_particles / (2 * path.τ) / path.n_beads
+	return prefactor * kinetic_energy + path.n_dimensions * path.n_particles / (2 * path.τ)
 end
 
 function potential_energy(path::Path, potential::ConstantPotential)
@@ -15,20 +16,24 @@ end
 
 function potential_energy(path::Path, potential::OneBodyPotential)
     potential_energy = 0.0
-    for bead in 1:path.n_beads, particle in 1:path.n_particles
+    bead = rand(1:path.n_beads)
+    for particle in 1:path.n_particles
         potential_energy += one_body_potential(potential, path, bead, particle)
     end
-    return potential_energy / path.n_beads
+
+    # potential_energy = primitive_inter_action(path, bead, bead-1, 1, potential) / path.τ
+    return potential_energy
 end
 
 function potential_energy(path::Path, potential::TwoBodyPotential)
     potential_energy = 0.0
-    for bead in 1:path.n_beads, particle_one in 1:path.n_particles, particle_two in 1:path.n_particles
+    bead = rand(1:path.n_beads)
+    for particle_one in 1:path.n_particles, particle_two in 1:path.n_particles
         if particle_one != particle_two
             potential_energy += two_body_potential(potential, path, bead, particle_one, particle_two)
         end
     end
-    return potential_energy / path.n_beads
+    return potential_energy
 end
 
 function potential_energy(path::Path, potentials::Array{Potential})
@@ -40,7 +45,7 @@ function potential_energy(path::Path, potentials::Array{Potential})
 end
 
 function Energy(path::Path, potential::Potential)
-    return kinetic_energy(path, potentials) + potential_energy(path, potential)
+    return kinetic_energy(path, potential) + potential_energy(path, potential)
 end
 
 function Energy(path::Path, potentials::Array{Potential})
@@ -50,6 +55,16 @@ function Energy(path::Path, potentials::Array{Potential})
     end
     return total_energy
 end
+
+# function Virial_Energy(path::Path, potential::Potential; window_size = 1)
+#     window_size = path.n_beads
+#     total_energy = path.n_dimensions * path.n_particles / (2 * window_size * path.τ * path.n_beads)
+#     general_force = 0.0
+#     for bead in 1:path.n_beads, particle in 1:path.n_particles
+#         general_force -= primit
+#     end
+
+# end
 
 function Correlation(path::Path, potential::Potential)
     correlation = Vector{Float64}(undef, path.n_beads)
