d84167582d84cd39be27797d24aabc09d7fdca46
george su <georgesuhk@gmail.com>
2022-07-28 12:45:25 +0100

    added possibility of weighted combination of movers

---
+++ test/EstimatorTest.jl
@@ -0,0 +1,81 @@
+begin
+using Revise
+using PolaronQMC
+using Statistics
+using Plots
+end
+
+begin
+
+#initialising variables
+
+    #for Potential
+    ω = 1.0
+
+    #for path
+    T = 1.0
+    λ = 0.5
+    m = 1.0
+    n_beads = 100
+    τ = 1.0 / (T * n_beads)
+    n_particles = 1
+    start_range = 1.0
+
+
+    #for analytic energy
+    ħ = 1.0
+    β = 1/T
+
+    #for pimc
+    n_steps = 100
+    equilibrium_skip = 0.1*n_steps
+    observables_skip = 0.01*n_steps
+    movers = [Displace!]
+    observables = [Energy]
+    
+    #potential type
+        potential = HarmonicPotential(ω)
+    
+    #estimator type
+        #estimator = Simple_Estimator()
+        #estimator = Thermodynamic_Estimator()
+        estimator = Virial_Estimator(n_beads)
+
+    #regime type
+        regime = Primitive_Regime()
+
+
+
+#running sim
+
+path = Path(n_beads, n_particles, τ, m = m, λ = λ, start_range = start_range)
+pimc = PIMC(n_steps::Int, equilibrium_skip, observables_skip, path, movers, observables, estimator, potential, regime)
+acceptance_ratio = pimc[1]
+output_observables = pimc[2]
+
+energy = output_observables["Energy"]
+analytic_energy = analytic_energy_harmonic(potential,β,ħ)
+
+
+#post analysis
+
+println("acceptance ratio -", acceptance_ratio)
+println("Mean energy = ", mean(energy))
+println("analytic energy = ", analytic_energy)
+println("average position = ", mean(path.beads[:,1]))
+
+
+#Plots
+worldline = plot(path.beads[:,1], 1:path.n_beads)
+energyplot = plot(energy)
+
+plot(worldline, energyplot, layout = (2,1), legend = false)
+
+
+
+
+
+
+
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using Test
+
+using PolaronQMC
+
+@assert 1==1
+
+#const T=300
+#const tau=0.5
+#nbeads=round(Int,1/(tau*T)) # ??? 
+
+#nbeads=4
+#nparticles=2
+#spatialdimensions=1
+
+#PolaronQMC.Path(nbeads, nparticles,  spatialdimensions=spatialdimensions)
+@testset "SimpleIntegrationTest.jl" begin include("SimpleIntegrationTest.jl") end
+
+@testset "BenchmarkMoves.jl" begin include("BenchmarkMoves.jl") end
+
+
