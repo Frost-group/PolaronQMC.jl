bdf9090bf7002e97e1b26f9959a1dd97fa3b6eac
george su <georgesuhk@gmail.com>
2022-08-03 15:31:50 +0100

    Bisect code finished, need to debug

--- src/moves.jl
+++ src/moves.jl
@@ -72,44 +72,48 @@ function Bisect!(path::Path, particle::Int, potential::Potential, regime::Regime
 	max_level = Int(floor(log(segment_length)/log(2))) # = 4 in arbitary setting
 
 	start_bead = rand(1:path.n_beads)
+	#println("sb = ", start_bead)
+	#println("ed = ",start_bead+segment_length)
 
 	old_beads = copy(path.beads[:,particle])
 
 
 	total_old_action = 0.0
-	for bead in start_bead:start_bead+segment_length-1
-		total_old_action += total_action(path, bead, bead+1, particle, potential, regime)
+	for bead in start_bead:start_bead+segment_length
+		total_old_action += potential_action(path, bead, particle, potential, regime)
 	end
 
 
-	segment_old_action = 0.0 # old action of the cut out segment
-	segment_new_action = 0.0 # new action of the cut out segment
+
 	for level in max_level-1:-1:0
+		segment_old_action = 0.0 # old action of the cut out segment
+		segment_new_action = 0.0 # new action of the cut out segment
 
 		
 		ratio = (segment_length - 1) / 2^level #how many divisions of level makes up full segment
-
-		for interval in 1:ratio-1
+		
+		for interval in 1:ratio
 			bead = Int(start_bead + (2^level * interval))
-			segment_old_action += total_action(path, bead, bead+1, particle, potential, regime)
-			shift = adjuster.shift_width_array[string(level)]*2 #auto adjusting level specific width
+			#println("bead = ", bead)
+			segment_old_action += potential_action(path, bead, particle, potential, regime)
+			shift = rand([-1,1]) * rand() * sqrt((2^level)*path.τ*path.λ) #auto adjusting level specific width
 			path.beads[bead, particle] = 0.5 * (path.beads[bead - 2^level, particle] + path.beads[bead + 2^level, particle]) + shift
-			segment_new_action += total_action(path, bead, bead+1, particle, potential, regime)
+			segment_new_action += potential_action(path, bead, particle, potential, regime)
 		end
-		segment_action_diff = segment_new_action - segment_old_action
+		segment_action_diff = 2^(level)* path.τ * segment_new_action - segment_old_action
 		if rand() >= exp(-segment_action_diff)
-			adjuster.adjust_counter_array[string(level)] -= 1
+			#adjuster.adjust_counter_array[string(level)] -= 1
 			return false
 		else
-			adjuster.adjust_counter_array[string(level)] += 1
+			#adjuster.adjust_counter_array[string(level)] += 1
 
 		end
 
 	end
 
 	total_new_action = 0.0
-	for bead in start_bead:start_bead+segment_length-1
-		total_new_action += total_action(path, bead, bead+1, particle, potential, regime)
+	for bead in start_bead:start_bead+segment_length
+		total_new_action += potential_action(path, bead, particle, potential, regime)
 	end
 
 	if total_new_action - total_old_action < 0.0
@@ -127,6 +131,68 @@ function Bisect!(path::Path, particle::Int, potential::Potential, regime::Regime
 end
 
 
+
+function Bisect!(path::Path, particle::Int, potential::Potential, regime::Regime, adjuster::Adjuster)
+
+	#max_level = Int(floor(log(rand(1:path.n_beads)) / log(2)))
+	segment_length = 16 + 1 #temporary arbitary choice
+	max_level = Int(floor(log(segment_length)/log(2))) # = 4 in arbitary setting
+
+	start_bead = rand(1:path.n_beads)
+	#println("sb = ", start_bead)
+	#println("ed = ",start_bead+segment_length)
+
+	old_beads = copy(path.beads[:,particle])
+
+
+	total_old_action = 0.0
+	for bead in start_bead:start_bead+segment_length
+		total_old_action += potential_action(path, bead, particle, potential, regime)
+	end
+
+
+
+	for level in max_level:-1:1
+		segment_old_action = 0.0 # old action of the cut out segment
+		segment_new_action = 0.0 # new action of the cut out segment
+
+		
+		ratio = 2^(max_level - level) #how many divisions of level makes up full segment
+		
+		for k in 1:ratio
+			bead = Int(start_bead + (2^(level-1) * k))
+			#println("bead = ", bead)
+			segment_old_action += potential_action(path, bead, particle, potential, regime)
+			shift = rand([-1,1]) * rand() * sqrt( 2^(level-1) * path.τ * path.λ) 
+			path.beads[bead, particle] = 0.5 * (path.beads[bead - 2^(level-1), particle] + path.beads[bead + 2^(level-1), particle]) + shift
+			segment_new_action += potential_action(path, bead, particle, potential, regime)
+		end
+		segment_action_diff = 2^(level-1)* path.τ * (segment_new_action - segment_old_action)
+		if rand() > exp(-segment_action_diff)
+			#adjuster.adjust_counter_array[string(level)] -= 1
+			return false
+		end
+
+	end
+
+	total_new_action = 0.0
+	for bead in start_bead:start_bead+segment_length
+		total_new_action += potential_action(path, bead, particle, potential, regime)
+	end
+
+	if total_new_action - total_old_action < 0.0
+
+		return true
+	elseif rand() < exp(-(total_new_action - total_old_action))
+
+		return true
+	else
+		path.beads[:, particle] = old_beads
+		return false
+	end
+
+
+end
 
 
 
--- test/EstimatorTest.jl
+++ test/EstimatorTest.jl
@@ -14,10 +14,10 @@ begin
     α = 10.0
 
     #for path
-    T = 1.0
+    T = 0.01
     λ = 0.5
     m = ω
-    n_beads = 1000
+    n_beads = 100
     τ = 1.0 / (T * n_beads)
     n_particles = 1
     start_range = 1.0
@@ -28,14 +28,14 @@ begin
     ħ = 1.0
     β = 1/T
 
-    #for pimc
-    n_steps = 100000
-    #equilibrium_skip = 0.1*n_steps
-    equilibrium_skip = 0
-    bservables_skip = 0.01*n_steps
-    #observables_skip = 1000
-    movers = [[Bisect!],[1.0]]
-    adjusters = [Bisect_Adjuster(path)]
+    #for pimc00
+    n_steps = 10000
+    equilibrium_skip = 0.1*n_steps
+    #equilibrium_skip = 0
+    observables_skip = 0.01*n_steps
+    #observables_skip = 100
+    movers = [[Single!],[1.0]]
+    adjusters = [Single_Adjuster(path)]
 
     observables = [Energy]
     
@@ -45,10 +45,10 @@ begin
         potential = HarmonicPotential(ω)
     
     #estimator type
-    estimators = [Virial_Estimator(500)]
+    #estimators = [Virial_Estimator(n_beads)]
         #estimator = Simple_Estimator()
-        #estimator = Thermodynamic_Estimator()
-        #estimator = Virial_Estimator(100)s
+        estimator = [Thermodynamic_Estimator()]
+        #estimator = Virial_Estimator(n_beads)
 
     #regime type
         regime = Primitive_Regime()
@@ -58,21 +58,21 @@ begin
 #running sim
 
 
-pimc = PIMC(n_steps::Int, equilibrium_skip, observables_skip, path, movers, observables, estimators, potential, regime, adjusters, adjust=true)
+pimc = PIMC(n_steps::Int, equilibrium_skip, observables_skip, path, movers, observables, estimators, potential, regime, adjusters, adjust=false)
 acceptance_ratio = pimc[1]
 output_observables = pimc[2]
 
 energy = output_observables["Energy"][string(Symbol(estimators[1]))]
-#analytic_energy = analytic_energy_harmonic(potential,β,ħ)
-#variances = jackknife(energy)
+analytic_energy = analytic_energy_harmonic(potential,β,ħ)
+variances = jackknife(energy)
 
 
 #post analysis
 
 println("acceptance ratio -", acceptance_ratio)
 println("Mean energy = ", mean(energy))
-#println("analytic energy = ", analytic_energy)
-#println("jackknife errors = ", sqrt(variances[2]))
+println("analytic energy = ", analytic_energy)
+println("jackknife errors = ", sqrt(variances[2]))
 
 
 
