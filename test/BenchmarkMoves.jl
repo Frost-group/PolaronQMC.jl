26d476e373b712f96ba4cb3d233d1e81008ed193
George <georgesuhk@gmail.com>
2022-08-10 16:34:11 +0100

    changing the way estimators are written to prepare for multi threading

--- test/BenchmarkMoves.jl
+++ test/BenchmarkMoves.jl
@@ -5,7 +5,10 @@ T = 1.0
 n_beads = 100
 τ = 1.0 / (T * n_beads)
 n_particles = 1
-path = PolaronQMC.Path(n_beads, n_particles, τ = τ, λ = λ)
+n_dimensions = 3
+
+
+path = PolaronQMC.Path(n_beads, n_particles, n_dimensions, τ = τ)
 
 println("Benchmarking PIMC moves...")
 println("Particles: $n_particles Beads: $n_beads ")
