8abd1b6926608b21d476bcd43fdebf0b8f0e7af0
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2022-11-16 16:41:23 +0000

    Making a PIMC function neat

--- analysis/GeneralPIMC.jl
+++ analysis/GeneralPIMC.jl
@@ -2,36 +2,31 @@ using Revise
 using PolaronQMC
 using Statistics
 using Plots
+include("../src/PolaronQMCVisualisation.jl")
+using .PolaronQMCVisualisation
 using PolaronMobility
+using LaTeXStrings
 
 
-function generalPIMC(T, m, n_particles, n_dimensions, start_range)
+function generalPIMC(T, m, n_particles, n_dimensions, regime, fixing_tau, fixed_τ, n_beads, n_steps, n_thermalised, movers, start_range = 1.0)
     
     """
     Initialise System Variables
     """
 
     # Path variables
-    T = 0.1
-    m = 1.0
-    n_particles = 1
-    n_dimensions = 1
-    start_range = 1.0
     β = 1 / T
             
     # For fixed τ 
-    fixed_τ = 0.01
-    adjusted_beads = Int(floor(1/(fixed_τ*T)))
+    if fixing_tau
+        adjusted_beads = Int(floor(1/(fixed_τ*T)))
+        path = Path(adjusted_beads, n_particles, n_dimensions, fixed_τ)
 
     # For fixed number of beads
-    n_beads = 200
-    τ = 1.0 / (T * n_beads)
-
-    # path = Path(n_beads, n_particles, n_dimensions, τ, m = m)
-    path = Path(adjusted_beads, n_particles, n_dimensions, fixed_τ)
-
-    # Set regime
-    regime = Primitive_Regime()
+    else
+        τ = 1.0 / (T * n_beads)
+        path = Path(n_beads, n_particles, n_dimensions, τ, m = m)
+    end
 
     """
     Set Potential Function
@@ -51,19 +46,15 @@ function generalPIMC(T, m, n_particles, n_dimensions, start_range)
     PIMC Variables
     """
 
-    #number of steps
-    n_steps = 200000
-
     #skipping between sampling
-    equilibrium_skip = 0.5 * n_steps
+    # equilibrium_skip = 0.5 * n_steps
+    equilibrium_skip = 0.1 * n_steps
     #equilibrium_skip = 0
-    observables_skip = 0.001 * n_steps
+    #observables_skip = 0.001 * n_steps
+    observables_skip = 500
     #observables_skip = 10 * n_steps
 
-    #types of moves
-    #movers = [[Bisect!],[1.0]]
-    movers = [[Single!],[1.0]]
-    #movers = [[Displace!,Single!],[0.2,1.0]]
+    
 
     observables = [Energy, Position]
     
@@ -75,7 +66,7 @@ function generalPIMC(T, m, n_particles, n_dimensions, start_range)
     Run Simulation
     """
 
-    #thermalised_start!(path, potential, n_steps = 100000)
+    thermalised_start!(path, potential, n_steps = n_thermalised)
     pimc = PIMC(n_steps, equilibrium_skip, observables_skip, path, movers, observables, estimators, potential, regime, adjust=true, visual=false)
     acceptance_ratio = pimc[1]
     output_observables = pimc[2]
@@ -97,4 +88,19 @@ function generalPIMC(T, m, n_particles, n_dimensions, start_range)
 end
 
 
-generalPIMC()
+#types of moves
+#movers = [[Bisect!],[1.0]]
+movers = [[Single!],[1.0]]
+#movers = [[Displace!,Single!],[0.2,1.0]]
+generalPIMC(0.1, #Temperature
+            1, # mass
+            1, # no of particles
+            1, # number of n_dimensions
+            Simple_Regime(), # regime type
+            true, # fixing tau or not
+            0.4, # fixed_τ
+            200, #n_beads of tau not fixed
+            10000000, # No. of steps
+            100000, # number of thermalisation
+            movers
+            )
