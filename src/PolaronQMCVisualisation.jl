7b1b9e519bb5b96260e7065d10068fc25c47b836
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2022-11-17 13:24:12 +0000

    Visual

--- src/PolaronQMCVisualisation.jl
+++ src/PolaronQMCVisualisation.jl
@@ -1,12 +1,12 @@
 # PolaronQMCVisualisation.jl
 using Revise
 using Plots
+using LaTeXStrings
 
 
-function draw_beads_3d(beads, xlims, ylims, zlims, n_particles, frame)
+function draw_beads_3d(beads, xlims, ylims, zlims, n_particles, frame, potential, mover, T)
 
-
-	p = plot()
+	p = plot(xlabel=L"x",ylabel=L"y", zlabel=L"z")
 	
 	for particle in 1:n_particles
 
@@ -23,14 +23,14 @@ function draw_beads_3d(beads, xlims, ylims, zlims, n_particles, frame)
 		push!(z, z[1])
 
 		plot!(p, x, y, z, marker = :circle, label = "P $particle", legend = false, xlims = xlims, ylims = ylims, zlims = zlims)
-		title!("PIMC Animation [Frame=$(frame)]")
+		title!("PIMC Animation [Frame=$(frame)] \n $(potential) \n $(mover) \n T=$(T)", title_position=:left)
 
 	end
 	return p
 end
     
 
-function animate_PIMC(pimc, n_particles)
+function animate_PIMC(pimc, n_particles, potential, mover, T)
 
 	xlims = [minimum([minimum(x[:, :, 1]) for x in pimc[3]]), maximum([maximum(x[:, :, 1]) for x in pimc[3]])]
 	ylims = [minimum([minimum(x[:, :, 2]) for x in pimc[3]]), maximum([maximum(x[:, :, 2]) for x in pimc[3]])]
@@ -38,7 +38,7 @@ function animate_PIMC(pimc, n_particles)
 
 	frame = 0
 	animation = Plots.@animate for p in pimc[3]
-		draw_beads_3d(p, xlims, ylims, zlims, n_particles, frame)
+		draw_beads_3d(p, xlims, ylims, zlims, n_particles, frame, potential, mover, T)
 		frame += 1
 	end
 
