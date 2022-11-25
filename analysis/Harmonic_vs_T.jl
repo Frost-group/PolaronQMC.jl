53dc39fef47be1d2b838eaa65e88b2f091ba8334
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2022-11-25 01:42:59 +0000

    harmonic vs T commit

--- analysis/Harmonic_vs_T.jl
+++ analysis/Harmonic_vs_T.jl
@@ -19,18 +19,22 @@ begin
     movers = [[Single!],[1.0]]
     #movers = [[Displace!,Single!],[0.2,1.0]]
     Potential = "Harmonic"
+    #Estimator = "Thermodynamic"
     Estimator = "Virial"
-    T_arr = 0.1:0.1:0.8
+    T_arr = 0.1:0.1:1.0
+    #T_arr = 10 .^ range(-2, 2, length = 8)
+    delta_tau = 0.2
+    #T_arr = [0.2] # for a single test
     Mean_energy_arr = [0.0 for i in 1:length(T_arr)]
     Error_arr = [0.0 for i in 1:length(T_arr)]
+    
     threads = true # Can turn it off by setting to false, if thread turn on cannot do plot inside function generalPIMC
-    #Mean_energy_arr = []
-    #Error_arr = []
+    #threads = false
     if threads
         @threads for i in 1:length(T_arr)
             println("Temp is", T_arr[i])
             # println("i = $(T_arr[i]) on thread $(Threads.threadid())")
-            energy, variances = generalPIMC(T_arr[i], #Temperature
+            energy, variances, accept_ratio = generalPIMC(T_arr[i], #Temperature
                                             1.0, # mass
                                             1.0, # ω (has to be float)
                                             1.0, # α (has to be float)
@@ -38,13 +42,13 @@ begin
                                             1, # number of n_dimensions
                                             Simple_Regime(), # regime type
                                             true, # fixing tau or not
-                                            0.4, # fixed_τ
+                                            delta_tau, # fixed_τ
                                             200, # n_beads of tau not fixed
-                                            100000, # No. of steps
-                                            10000, # number of thermalisation
+                                            1000000, # No. of steps
+                                            100000, # number of thermalisation
                                             movers, # movers
-                                            "Harmonic", # potential type
-                                            "Virial", # estimators
+                                            Potential, # potential type
+                                            Estimator, # estimators
                                             threads # not threading
                                             )            
             #push!(Mean_energy_arr, mean(energy))
@@ -56,7 +60,7 @@ begin
         for i in 1:length(T_arr)
             println("Temp is", T_arr[i])
             # println("i = $(T_arr[i]) on thread $(Threads.threadid())")
-            energy, variances = generalPIMC(T_arr[i], #Temperature
+            energy, variances, accept_ratio = generalPIMC(T_arr[i], #Temperature
                                             1.0, # mass
                                             1.0, # ω (has to be float)
                                             1.0, # α (has to be float)
@@ -64,13 +68,13 @@ begin
                                             1, # number of n_dimensions
                                             Simple_Regime(), # regime type
                                             true, # fixing tau or not
-                                            0.4, # fixed_τ
+                                            delta_tau, # fixed_τ
                                             200, # n_beads of tau not fixed
-                                            100000, # No. of steps
-                                            10000, # number of thermalisation
+                                            1000000, # No. of steps
+                                            100000, # number of thermalisation
                                             movers, # movers
-                                            "Harmonic", # potential type
-                                            "Virial", # estimators
+                                            Potential, # potential type
+                                            Estimator, # estimators
                                             threads # not threading
                                             )         
             #push!(Mean_energy_arr, mean(energy))
@@ -84,14 +88,15 @@ end
 begin
     hbar = 1
     omega = 1
-    T_arr = 0.1:0.1:0.8
+    #T_arr = 0.1:0.1:0.8
     Analytical_QHO(x) = hbar*omega/2 .+ hbar*omega*exp.(-omega*hbar ./ x)./(1 .-exp.(-hbar*omega ./ x))
     Energy_plot = plot(Analytical_QHO, 0.1, 1, labels="analytical", legend=:topleft)  
     
     scatter!(T_arr,
             Mean_energy_arr,
-            labels = "data",
+            labels = Estimator,
             legend=:topleft,
+            xaxis=:log,
             yerror = Error_arr,
             markerstrokewidth=3,
             markercolor = "Red",
@@ -101,5 +106,7 @@ begin
     #scatter!(T_arr, Comparison_energy_arr, labels = "Theory", markerstrokewidth=0, markercolor = "Red",)
     title!("Energy vs Temperature")
     display(Energy_plot)
-    #savefig("../figs/HarmonicEnergyVSTemperature.png")
+    #savefig("./figs/HarmonicEnergyVSTemperature_$(Estimator)_deltatau=$(delta_tau).png")
+    #savefig("../figs/HarmonicEnergyVSTemperature_$(Estimator).png")
+    #savefig("./figs/HarmonicEnergyVSTemperature_break_down.png")
 end
