c25780ccdfa24c2867a978e73570f10ec38353ff
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2022-12-02 08:55:19 +0000

    Update potentials.jl
    Frohlich parameter

--- src/potentials.jl
+++ src/potentials.jl
@@ -25,12 +25,13 @@ function one_body_potential(potential::FrohlichPotential, path::Path, bead::Int,
     for other_bead in 1:path.n_beads
         if other_bead != bead
             #g_factor = -potential.α/2 * sqrt(potential.ħ/(2*path.m*potential.ω)) * cosh(β *(abs(bead-other_bead)/path.n_beads - 0.5)) * csch(β/2)
-            g_factor = -0.5 * potential.α * (potential.ħ * potential.ω)^3/2 * sqrt(2*path.m) * cosh(potential.ω*β * (abs(bead-other_bead)/path.n_beads - 0.5 * potential.ħ)) * csch(potential.ħ * potential.ω * β / 2)
+            #g_factor = -0.5 * potential.α * (potential.ħ * potential.ω)^3/2 * sqrt(2*path.m) * cosh(potential.ω*β * (abs(bead-other_bead)/path.n_beads - 0.5 * potential.ħ)) * csch(potential.ħ * potential.ω * β / 2)
 
+            g_factor = -0.5 * potential.α * (potential.ħ * potential.ω)^3/2 / pi * sqrt(2/path.m) * cosh(potential.ω * β * potential.ħ * (abs(bead-other_bead)/path.n_beads - 0.5))* csch(potential.ħ * potential.ω * β / 2)
             inner_integral += g_factor / norm(path.beads[bead, particle, :] - path.beads[other_bead, particle, :])
         end
     end
-    return path.τ * inner_integral
+    return path.τ * inner_integral # Note that this path.τ multiplication refer to dτ'
 end
 
 
