e52bde36a8ba5120f9a7929a71a5e99890920c50
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2023-01-12 12:04:49 +0000

    General test

--- examples/GeneralTest.jl
+++ examples/GeneralTest.jl
@@ -6,14 +6,14 @@ using PolaronMobility
 using LaTeXStrings
 
 
-begin
+@time begin
     
     """
     Initialise System Variables
     """
 
     # Path variables
-    T = 0.025
+    T = 0.8
     m = 1.0
     n_particles = 1
     n_dimensions = 3
@@ -25,7 +25,7 @@ begin
     adjusted_beads = Int(floor(1/(fixed_τ*T)))
 
     # For fixed number of beads
-    n_beads = 50
+    n_beads = 40
     τ = 1.0 / (T * n_beads)
 
     path = Path(n_beads, n_particles, n_dimensions, τ, m = m)
@@ -53,14 +53,14 @@ begin
     """
 
     # number of steps
-    n_steps = 10000
+    n_steps = 2000
 
 
     #skipping between sampling
     equilibrium_skip = 0.5 * n_steps
     #equilibrium_skip = 1
-    observables_skip = 0.01 * n_steps
-    #observables_skip = 1
+    #observables_skip = 0.001 * n_steps
+    observables_skip = 10
 
     # types of moves
     movers = Dict("Bisect!" => [1.0])
@@ -134,21 +134,21 @@ begin
     energy_plot = plot(energies, ylabel="Energy", xlab = "Sweeps / $observables_skip\$ n\$")
     hline!([comparison_energy], linestyle=:dash)
     energy_hist = histogram(energies, ylab="Frequencies", xlab="Energy")
-    acceptance_rate_plot = plot(acceptance_rates, xlab = L"\mathrm{Sweeps\, /\, } n", ylab=L"\mathrm{Acceptance\, Rate\, /\, } r", dpi=600)
-    shift_width_plot = plot(shift_widths, xlab = L"\mathrm{Sweeps\, /\, } n", ylab=L"\mathrm{Shift\, Width\, /\, } \Delta x", dpi=600)
-    acceptance_shift_plot = scatter(acceptance_rates, shift_widths, xlab=L"\mathrm{Acceptance\, Rate\, /\, } r", ylab=L"\mathrm{Shift\, Width\, /\, } \Delta x", dpi=600)
+    #acceptance_rate_plot = plot(acceptance_rates, xlab = L"\mathrm{Sweeps\, /\, } n", ylab=L"\mathrm{Acceptance\, Rate\, /\, } r", dpi=600)
+    #shift_width_plot = plot(shift_widths, xlab = L"\mathrm{Sweeps\, /\, } n", ylab=L"\mathrm{Shift\, Width\, /\, } \Delta x", dpi=600)
+    #acceptance_shift_plot = scatter(acceptance_rates, shift_widths, xlab=L"\mathrm{Acceptance\, Rate\, /\, } r", ylab=L"\mathrm{Shift\, Width\, /\, } \Delta x", dpi=600)
     acceptance_rate_hist = histogram(acceptance_rates, ylab="Frequency", xlab=L"\mathrm{Acceptance\, Rate\, /\, } r")
-    shift_width_hist = histogram(shift_widths, ylab="Frequency", xlab=L"\mathrm{Shift\, Width\, /\, } \Delta x")
+    #shift_width_hist = histogram(shift_widths, ylab="Frequency", xlab=L"\mathrm{Shift\, Width\, /\, } \Delta x")
     #posplot = histogram(position[:,1,1])
     #plot(posplot, energyplot, layout = (2,1), legend = false)
     #plot(posplot, xlabel="Position", ylabel="Prob Amplitude", legend = false)
 
     display(energy_hist)
     display(energy_plot)
-    display(acceptance_rate_plot)
-    display(shift_width_plot)
-    display(acceptance_shift_plot)
-    display(shift_width_hist)
+    #display(acceptance_rate_plot)
+    #display(shift_width_plot)
+    #display(acceptance_shift_plot)
+    #display(shift_width_hist)
     display(acceptance_rate_hist)
 
     #savefig(energy_hist, "saved_plots/energy_hist.png") 
