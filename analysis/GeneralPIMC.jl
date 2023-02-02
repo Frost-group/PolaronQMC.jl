df85ecf062736814f6c7837db7521f95f3752343
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2023-02-02 13:24:38 +0000

    Update GeneralPIMC.jl

--- analysis/GeneralPIMC.jl
+++ analysis/GeneralPIMC.jl
@@ -5,6 +5,124 @@ using Plots
 using PolaronMobility
 using LaTeXStrings
 
+#=
+function generalPIMC(T, m, ω, α, n_particles, n_dimensions, regime, fixing_tau, fixed_τ, n_beads, n_steps, n_thermalised, movers, potential, estimator, threads::Bool = true, start_range = 1.0)
+    
+    """
+    Initialise System Variables
+    """
+
+    # Path variables
+    β = 1 / T
+    ħ = 1.0 
+    # For fixed τ 
+    if fixing_tau
+        adjusted_beads = Int(floor(1/(fixed_τ*T)))
+        if adjusted_beads < 1
+            adjusted_beads = 1
+        end
+        path = Path(adjusted_beads, n_particles, n_dimensions, fixed_τ)
+
+    # For fixed number of beads
+    else
+        τ = 1.0 / (T * n_beads)
+        path = Path(n_beads, n_particles, n_dimensions, τ, m = m)
+    end
+
+    if threads
+        plot_on = false
+    else
+        plot_on = true
+    end
+    """
+    Set Potential Function
+    """
+    
+    if potential == "Frohlich"
+        potential = FrohlichPotential(α,ω,ħ) # ω is phonon frequency
+    elseif potential == "Harmonic"
+        potential = HarmonicPotential(ω)
+    elseif potential == "MexicanHat"
+        potential = MexicanHatPotential(80000.0)
+    elseif potential == "Constant"
+        potential = ConstantPotential(10.0)
+    end
+
+    """
+    PIMC Variables
+    """
+
+    #skipping between sampling
+    equilibrium_skip = 0.4 * n_steps
+    observables_skip = 100
+    #observables_skip = 0.001 * n_steps
+
+    observables = [Energy, Position]
+    
+    if estimator == "Virial"
+        estimators = [Virial_Estimator()]
+
+    elseif estimator == "Thermodynamic"
+        estimators = [Thermodynamic_Estimator()]
+    
+    elseif estimator == "Simple"
+        estimators = [Simple_Estimator()]
+    end
+    """
+    Run Simulation
+    """
+
+    #thermalised_start!(path, potential, n_steps = n_thermalised)
+    pimc = PIMC(n_steps, equilibrium_skip, observables_skip, path, movers, observables, estimators, potential, regime, adjust=true, visual=false)
+    adjuster_stats = pimc[1]
+    output_observables = pimc[2]
+
+    energy = output_observables["Energy"][string(Symbol(estimators[1]))];
+    position = output_observables["Position"][string(Symbol(estimators[1]))];
+
+    # Comparison energy
+    if typeof(potential) == HarmonicPotential
+        comparison_energy = analytic_energy_harmonic(potential,β,ħ, n_dimensions)
+    elseif typeof(potential) == FrohlichPotential
+        comparison_polaron = make_polaron([α], [T], [0.0]; ω=1.0, rtol = 1e-4, verbose = true, threads = true) # orginally threads is true
+        comparison_energy = -comparison_polaron.F
+        #comparison_energy = 0.0
+    end
+
+    variances = jackknife(energy)
+
+    # Post analysis
+    acceptance_rates = adjuster_stats["Single!"]["Acceptance Rate"]
+    last_acceptance_rate = last(acceptance_rates)
+    mean_acceptance_rate = mean(acceptance_rates)
+    std_acceptance_rate = std(acceptance_rates)
+
+    println("acceptance ratio = ", last_acceptance_rate)
+    println("Mean Acceptance Rate: ", mean_acceptance_rate, " +/- ", std_acceptance_rate)
+    println("Mean energy = ", mean(energy))
+    println("comparison_energy = ", comparison_energy)
+    println("jackknife errors = ", sqrt(variances[2]))
+    println("number of energy ", length(energy))
+    
+
+    #Plots
+    
+    if plot_on
+        energyplot = scatter(energy[Int(floor(0.8*length(energy))):end], ylabel="Energy", xlabel="x * $observables_skip steps", labels = estimator)
+        #energyplot = scatter(energy, ylabel="Energy", xlabel="x * $observables_skip steps")
+        title!("Energy against steps")
+        display(energyplot)
+        savefig("./figs/Energy_conv/Energyvssteps_T=$(T)_energy=$(estimator).png")
+    end
+    
+    #posplot = histogram(position[:,1,1])
+    #plot(posplot, energyplot, layout = (2,1), legend = false)
+    #plot(posplot, xlabel="Position", ylabel="Prob Amplitude", legend = false)
+
+    return energy, variances, mean_acceptance_rate, comparison_energy
+
+end
+=#
 
 function generalPIMC(T, m, ω, α, n_particles, n_dimensions, regime, fixing_tau, fixed_τ, n_beads, n_steps, n_thermalised, movers, potential, estimator, threads::Bool = true, start_range = 1.0)
     
