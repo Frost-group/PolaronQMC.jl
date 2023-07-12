75e7c1e873de26bcc858003d961b50753af10f4b
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2023-07-12 16:54:11 +0100

    Update PolaronTest.jl

--- examples/PolaronTest.jl
+++ examples/PolaronTest.jl
@@ -14,13 +14,13 @@ using JLD
     """
 
     # Randomly select a version number for saving purposes
-    version = Int(floor(rand()*1000))
+    version = Int(floor(rand()*10000))
 
     # Set Parameters, all use atomic units where m = ħ = ω = 1.0
-    T = 0.1
+    T = 0.15
     m = 1.0
     ω = 1.0
-    α = 5.0
+    α = 8.0
     ħ = 1.0
 
     n_particles = 1
@@ -29,7 +29,7 @@ using JLD
     β = 1 / T
 
     # Number of Monte-Carlo-Steps
-    n_steps = 1000
+    n_steps = 15000
 
     # Choose potential from "Harmonic", "Frohlich", "MexicanHat", "Constant"
     potential = "Frohlich"
@@ -58,18 +58,18 @@ using JLD
         τ = 1.0 / (T * n_beads)
     else
         # For fixed τ
-        τ = 0.005
+        τ = 0.015
         n_beads = Int(floor(1/(τ*T)))
     end
 
     # Fixed observable skip or step dependant
     quick_steps = false
     if quick_steps
-        equilibrium_skip = 50
-        observables_skip = 50
+        equilibrium_skip = 1
+        observables_skip = 1
     else
         equilibrium_skip = 0.5 * n_steps #try to put as 0.5, for 0.2 for quicker testing process
-        observables_skip = 0.001 * n_steps
+        observables_skip = 0.002 * n_steps
     end
 
     # Initate path
@@ -126,6 +126,8 @@ using JLD
     """
 
     println("started T is ", T)
+    println("started α is ", α)
+    println("n_step is ", n_steps)
     # thermalised_start!(path, potential, n_steps = 100000)
     data = PIMC(n_steps, equilibrium_skip, observables_skip, path, mover, estimators, potential, regime, observables, adjust=true)
     
@@ -151,16 +153,14 @@ using JLD
     mean_energy = mean(energies)
     corr_mean = mean(correlations)
     corr_std = std(correlations)
-    #last_acceptance_rate = last(acceptance_rates)
-    #mean_acceptance_rate = mean(acceptance_rates)
-    #std_acceptance_rate = std(acceptance_rates)
 
     # Saving data in a big jld file (dictionary)
-    save("data_arr/$(pot)/$(string(typeof(potential)))_T$(T)_nsteps$(n_steps)_v$(version)_beads$(n_beads).jld", "data", data, "energies", energies, "comparison_energy", comparison_energy, "correlations", correlations, "jacknife_errors", jacknife_errors, 
-                "equilibrium_skip", equilibrium_skip, "observables_skip", observables_skip)
+    save("data_arr/$(pot)/$(string(Symbol(potential)))_T$(T)_nsteps$(n_steps)_v$(version)_beads$(n_beads).jld", "data", data, "energies", energies, "comparison_energy", comparison_energy, "correlations", correlations, "jacknife_errors", jacknife_errors, 
+                "equilibrium_skip", equilibrium_skip, "observables_skip", observables_skip, "final_pos", path.beads[:, :, :])
     
 
     # Output measurements and statistics
+    println("version is:", version)
     println("Number of Beads: ", n_beads)
     println("Number of steps: ", n_steps)
     println("τ is: ", τ)
@@ -173,7 +173,7 @@ using JLD
     #println("Mean Acceptance Rate: ", mean_acceptance_rate, " +/- ", std_acceptance_rate)
 
     # Define plot parameters
-    default(fontfamily="Computer Modern",
+    default(fontfamily="Times New Roman",
         titlefont = (16, "Computer Modern"),
         guidefont = (18, "Computer Modern"),
         tickfont = (12, "Computer Modern"),
@@ -197,7 +197,7 @@ using JLD
     display(energy_hist)
     display(energy_plot)
     display(posplot)
-    display(corr_plot)
+    #display(corr_plot)
 
     if n_particles == 2
         positions1 = positions
@@ -226,21 +226,57 @@ using JLD
     #gif(anim, "saved_plots/anim_output.gif", fps = 60) 
 
 end
-
+#=
 begin
-    energies1 = []
-    for i in 1:length(energies)
-        if mod(i, 100) == 0
-            push!(energies1, energies[i])
-        end
-    end
-    autoCorrelation1 = autoCorrelation(energies1, observables_skip)
-    
-    l = length(energies1)-1
-    auto_plot = plot(1:l, autoCorrelation1[1:l], ylabel=L"C_{k}", xlab = "k", title="Autocorrelation time (α=2, T=0.1)", background_color = :transparent, foreground_color=:black)
-    display(auto_plot)
-    println("correlation time is:", autoCorrelationTime(autoCorrelation1))
-    #savefig("image/autocorrelation_100.png")
+    default(fontfamily="Computer Modern",
+        titlefont = (16, "Computer Modern"),
+        guidefont = (14, "Computer Modern"),
+        tickfont = (12, "Computer Modern"),
+        legendfontsize = 12,
+        linewidth=2, framestyle=:box, label=nothing, grid=true)
+    energy_plot = plot(energies[1:1000], ylabel="Energy", labels=L"α=1.0, T=0.1", xlab = "Steps", dpi=300)
+    hline!([comparison_energy], linestyle=:dash)
+    energy_plot2 = plot(energies[1:8000], ylabel="Energy", labels=L"α=1.0, T=0.1", xlab = "Steps", dpi=300)
+    hline!([comparison_energy], linestyle=:dash)
+    energy_hist = histogram(energies, ylab="Frequencies", labels=L"α=1.0, T=0.1", xlab="Energy", color=:lightblue, bins=70, dpi=300)
+    posplot = stephist(positions_flatten, xlab = "Position", labels=L"α=1.0, T=0.1", normalize=:pdf)
+    acceptance_rates = data["Acceptance Rate:p1"]
+    shift_width = data["Adjuster Value:p1"]
+    acceptance_rate_plot = plot(acceptance_rates[1:200], xlab = "Steps", ylab="Acceptance Rate", dpi=600)
+    shift_width_plot = plot(shift_width[1:200], xlab = "Steps", ylab=L"\mathrm{Shift\, Width} \ \Delta x", dpi=600)
+    acceptance_rate_hist = histogram(acceptance_rates, xlab = "Acceptance Rate", ylab="Frequencies", dpi=600, color=:skyblue, labels=L"α=1.0, T=0.1", bins=100)
+    shift_width_hist = histogram(shift_width, xlab = L"\mathrm{Shift\, Width }\ \Delta x", ylab="Frequencies", labels=L"α=1.0, T=0.1",color=:skyblue, dpi=600)
+    #display(posplot)
+    display(energy_plot)
+    display(energy_plot2)
+    display(energy_hist)
+    #display(shift_width_plot)
+    #display(acceptance_rate_plot)
+    #display(acceptance_rate_hist)
+    #display(shift_width_hist)
+
+    #savefig(acceptance_rate_hist, "report_image/acceptance_rate_shift_α1_T0.1.png")
+    #savefig(acceptance_rate_plot, "report_image/acceptance_rate_plot_α1_T0.1.png")
+    #savefig(shift_width_hist, "report_image/shift_width_hist_α1_T0.1.png")
+    #savefig(shift_width_plot, "report_image/shift_width_plot_α1_T0.1.png")
+    savefig(energy_plot, "report_image/energy_plot_α1_T0.1.png")
+    savefig(energy_plot2, "report_image/energy_plot2_α1_T0.1.png")
+    savefig(energy_hist, "report_image/energy_hist_α1_T0.1.png")
+
 end
+=#
+#=
+begin
+    energies2 = energies
+    autoCorrelation2 = autoCorrelation(energies2, 1.0)
+end
+begin
+    l = Int(floor(length(energies1)-1)/2)
+    auto_plot = plot(1:l, autoCorrelation2[1:l], ylabel=L"C_{j}", xlab = L"j  \ / \ steps", labels = L"α=1.0, T=0.1", dpi=600)#, title=L"Autocorrelation time (α=2, T=0.1)")#, background_color = :transparent, foreground_color=:black)
+    display(auto_plot)
+    println("correlation time is:", autoCorrelationTime(autoCorrelation1[1:7500]))
+    #savefig("report_image/autocorrelation_alpha1_T0.1_dpi600.png")
+end
+=#
 
 
