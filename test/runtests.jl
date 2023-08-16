408ed95aa485b3a4ab0adf880b796b760cf44aff
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2023-08-16 16:42:50 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,32 @@
 using Test
 
-using PolaronQMC
+#using PolaronQMC
 
-@assert 1==1
+function foo()
+    return 3
+end
 
-#const T=300
-#const tau=0.5
-#nbeads=round(Int,1/(tau*T)) # ??? 
-
-#nbeads=4
-#nparticles=2
-#spatialdimensions=1
+println(foo())
+#@assert 1==1
 
 #PolaronQMC.Path(nbeads, nparticles,  spatialdimensions=spatialdimensions)
-@testset "EstimatorTest.jl" begin include("EstimatorTest.jl") end
-
-
 
 
 #@testset "BenchmarkMoves.jl" begin include("BenchmarkMoves.jl") end
 
 
-@testset "BenchmarkMoves.jl" begin include("BenchmarkMoves.jl") end
+#@testset "BenchmarkMoves.jl" begin include("BenchmarkMoves.jl") end
+a = 3
+@test π≈3.14 atol=0.01 #quick one-line test
+@test 2 + 2 ≈ 6 atol=1 broken=true
+@testset foo()
 
+@testset "trigonometric identities" begin
+    θ = 2/3*π
+    @test sin(-θ) ≈ -sin(θ)
+    @test cos(-θ) ≈ cos(θ)
+    @test sin(2θ) ≈ 2*sin(θ)*cos(θ)
+    @test cos(2θ) ≈ cos(θ)^2 - sin(θ)^2
+end;
 
+@tes
