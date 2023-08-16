6d2dba05e1d6fe27f8fee0ae14238bf8bd276866
Jarvist Moore Frost <jarvist@gmail.com>
2023-08-16 16:15:47 +0100

    Revert "added benchmark tests for smaller functions within mover algorithms"
    This reverts commit c17b7578c103f1df5aa427051dda91461849cef0.

--- test/BenchmarkMoves.jl
+++ test/BenchmarkMoves.jl
@@ -2,71 +2,44 @@ begin
 using Revise
 using PolaronQMC
 using BenchmarkTools
+end
 
+begin
 
 #variables used
-    T = 3.0
-    β = 1/T
-    alpha_range = 1.0
-    thermalisation_steps = 100
-    steps_base = 800
-    fixed_τ = 0.0025
-    adjusted_beads = Int(floor(1/(fixed_τ*T)))
+T = 3.0
+β = 1/T
+alpha_range = 1.0
+thermalisation_steps = 100
+steps_base = 800
+fixed_τ = 0.0025
+adjusted_beads = Int(floor(1/(fixed_τ*T)))
 
-    path = Path(adjusted_beads, 1, 3, fixed_τ)
+path = Path(adjusted_beads, 1, 3, fixed_τ)
 
-    α = 1.0
-
-
-    println("Benchmarking PIMC moves...")
-    println("Beads: $adjusted_beads ")
-
-    particle = rand(1:path.n_particles)
-    bead = rand(1:path.n_beads)
+α = 1.0
 
 
-end
+println("Benchmarking PIMC moves...")
+println("Beads: $adjusted_beads ")
 
+particle = rand(1:path.n_particles)
 
-begin
+#potential = FrohlichPotential(α, 1.0, 1.0)
+potential = HarmonicPotential(1.0)
 
-println("PIMC")
-pimcx_bm = @btime PIMC(100, 100, 100, path, [[Bisect!],[1.0]], [], [], FrohlichPotential(α, 1.0, 1.0), Primitive_Regime())
-println("\n")
+# Could set histogram limits here etc. ; see
+# https://juliaci.github.io/BenchmarkTools.jl/stable/manual/#Visualizing-benchmark-results
+io=IOContext(stdout)
 
-println("PIMCX")
-pimcx_bm = @btime PIMCX(100, 100, 100, path, [[Bisect!],[1.0]], [], [], FrohlichPotential(α, 1.0, 1.0), Primitive_Regime())
-println("\n")
-
-end
-begin
-
-
-#Single
 println("Single!()")
-single_bm = @btime Single!(path, particle, FrohlichPotential(α, 1.0, 1.0), Primitive_Regime(), path.adjusters["Single!"])
-println("\n")
+s_bm = @benchmark Single!(path, particle, potential, Primitive_Regime(), path.adjusters["Single!"])
+show(io,MIME("text/plain"), s_bm)
 
-#Bisect
-println("Bisect!() Harmonic")
-bisect_bm = @btime Bisect!(path, particle, HarmonicPotential(1.0), Primitive_Regime(), path.adjusters["Bisect!"])
+println(" ")
 
-
-println("Bisect!() Frohlich")
-bisect_bm = @btime Bisect!(path, particle, FrohlichPotential(α, 1.0, 1.0), Primitive_Regime(), path.adjusters["Bisect!"])
-println("\n")
-
-
-
-#benchmarking smaller sections
-
-println("Frohlich one_body_potential")
-frohlich_bm = @btime one_body_potential(FrohlichPotential(α, 1.0, 1.0), path, bead, particle)
-println("\n")
-
-println("Deepcopy")
-deepcopy_bm = @btime deepcopy(path.beads[:,particle, :])
-println("\n")
-
-
+println("Bisect!()")
+b_bm = @benchmark Bisect!(path, particle, potential, Primitive_Regime(), path.adjusters["Bisect!"])
+show(io,MIME("text/plain"),b_bm)
 end
+
