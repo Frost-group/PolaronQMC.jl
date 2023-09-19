f2b82546d9156aa248c3fbd683075d757741a6a0
ycwong-chess <55613268+ycwong-chess@users.noreply.github.com>
2023-09-19 13:23:03 +0100

    Update FunctionTest.jl

--- test/FunctionTest.jl
+++ test/FunctionTest.jl
@@ -6,39 +6,17 @@ begin
     using PolaronMobility
     using LaTeXStrings
     using BenchmarkTools
-    include("GeneralPIMC.jl")
 end
 
 @time begin
-    n_steps = 100000;
-    T = 0.01;
-    mean_energy, jacknife_errors, comparison_energy, energies, positions, correlations, acceptance_rates, adjuster_values, equilibrium_skip, observables_skip, version, potential, n_beads, data = generalPIMC(
-                T, #Temperature
-                1.0, # mass
-                1.0, # ω (has to be float)
-                7.0, # α (has to be float)
-                1, # no of particles
-                3, # number of n_dimensions
-                "Primitive", # regime type
-                true, # fixing beads or not
-                0.04, # fixed_τ
-                200, # n_beads (if τ is not fixed)
-                n_steps, # No. of steps
-                50000, # number of thermalisation steps
-                "Single", # movers
-                "Frohlich", # potential type
-                "Virial", # estimators
-                false, # Not quick steps
-                false, # threading
-                1.0, # Start Range
-                1,
-                1, 
-                0.005, # observable skips
-                0.5, # equilibrium skips
-                1,
-                true,
-                1
-                );
+    n_steps = 500000;
+    est = "Thermodynamic"
+    #est = "Thermodynamic"
+    pot = "Harmonic";
+    T = 0.1; version=rand(1:10000)
+    data, energy, error, comparison_energy, equilibrium_skip, observables_skip, n_beads, path = 
+        generalPIMC(T, 1.0, 2.0, 3, 500000, version=1, pot=pot, estimator=est, fixed_τ=0.2)
+    println("-------simulation end------------")
 end
 
 begin
@@ -51,6 +29,9 @@ begin
         linewidth=2, framestyle=:box, label=nothing, grid=true)
 
     # Energy/Position Plots
+    positions = data["Position:p1d1"]
+    energies = data["Energy:$(est)"]
+
     positions_flatten = collect(Iterators.flatten(positions))
     posplot = histogram(positions_flatten, xlab = "Position")
     display(posplot)
@@ -58,7 +39,6 @@ begin
     hline!([comparison_energy], linestyle=:dash)
     energy_hist = histogram(energies, ylab="Frequencies", xlab="Energy")
     positions_flatten = collect(Iterators.flatten(positions))
-    #posplot = histogram(positions_flatten, xlab = "Position")
 
     # Correlation cut-off set by n
     #corr_plot = plot(1:n_beads-1, corr_mean[1:n_beads-1], yerror = corr_std, ylabel="G(Δτ)", xlabel = "Δτ")
@@ -74,5 +54,5 @@ begin
 
     #display(auto_plot)
 
-    SaveJLDData(T, potential, version, n_beads, n_steps, data)
+    #SaveJLDData(T, potential, version, n_beads, n_steps, data)
 end
