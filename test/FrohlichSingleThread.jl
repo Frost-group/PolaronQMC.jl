56e758c87ae82e869e1c0349f3ce23efbb6f1f09
Jarvist Moore Frost <jarvist@gmail.com>
2024-02-09 10:54:51 +0000

    Make it more like an actual test...

--- test/FrohlichSingleThread.jl
+++ test/FrohlichSingleThread.jl
@@ -1,22 +1,9 @@
-begin
-    using Revise
-    using PolaronQMC
-    using Statistics
-    using Plots
-    using PolaronMobility
-    using LaTeXStrings
-    using Distributions
-end
 
+pot = "Frohlich";
+T = 0.5; version=rand(1:10000)
+data, energy, error, comparison_energy, equilibrium_skip, observables_skip, n_beads, path = 
+    generalPIMC(T, 1.0, 2.0, 3, 100000, version=1, pot=pot)
 
-@time begin
-    pot = "Frohlich";
-    T = 0.5; version=rand(1:10000)
-    data, energy, error, comparison_energy, equilibrium_skip, observables_skip, n_beads, path = 
-        generalPIMC(T, 1.0, 2.0, 3, 100000, version=1, pot=pot)
-
-    println("-----Simulation Ended-----")
-end
-
+println("-----Simulation Ended-----")
 
 
